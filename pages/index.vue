c20675ea1dd2724491181c28ceb384ea7a00bd0a
Hudson Q <hudson@koipond.xyz>
2022-11-17 23:59:38 -0600

    font change / unknown

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-    <h1>you are forbidden from this pond</h1>
+    <h1 style="font-family:Inconsolata;">you are forbidden from this pond</h1>
     <div>
       <a href="/qr">become a koi</a>
     </div>
     <MyFancyButton>print money button</MyFancyButton>
 </template>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
-h1 {font-family: 'Roboto', sans-serif;}
+  @import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@200&family=Roboto:wght@100&display=swap');
 </style>
+
